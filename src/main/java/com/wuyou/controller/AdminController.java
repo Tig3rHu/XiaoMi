424dc95082e0b600fbf994438934e61540db88db
tigerOrtiger <1161629782@qq.com>
2018-11-03 20:48:26 +0800

    ceshi

--- src/main/java/com/wuyou/controller/AdminController.java
+++ src/main/java/com/wuyou/controller/AdminController.java
@@ -17,14 +17,20 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wuyou.entity.Admin;
 import com.wuyou.entity.SystemModule;
 import com.wuyou.entity.SystemModuleVo;
+import com.wuyou.entity.User;
 import com.wuyou.service.AdminService;
 import com.wuyou.service.SystemModuleService;
+import com.wuyou.service.UserService;
 
 /**
  * @author 吴优
@@ -32,7 +38,7 @@ import com.wuyou.service.SystemModuleService;
  * other:择日不如撞日
  * @date 2018年9月28日 
  */
-@Controller
+@RestController
 @RequestMapping("/admin")
 public class AdminController {
 	
@@ -40,6 +46,8 @@ public class AdminController {
 	private AdminService adminService;
 	@Autowired
 	private SystemModuleService systemModuleService;
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("/tologin")
 	public String index11(){
@@ -101,5 +109,11 @@ public class AdminController {
 	public String towelcome(){
 		return "towelcome/towelcome";
 	}
-
+	@GetMapping("/get/{id}")
+	@ResponseBody
+	public String get(@PathVariable("id") Integer id){
+		User u=userService.findById(id);
+	     
+	    return JSONObject.toJSONString(u);
+	}
 }
--- src/main/java/com/wuyou/entity/User.java
+++ src/main/java/com/wuyou/entity/User.java
@@ -15,6 +15,17 @@ public class User {
 	private String password;
 	
 	private String address;
+	
+	private String email;
+	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	public Integer getId() {
 		return id;
